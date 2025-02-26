66977d0ad7b65607fc69343a4925836f7437d56f
pushprajcrownstack <pushpraj@crownstack.com>
2025-02-26 13:32:01 +0530

    add signup api with email verification

--- app/api/users/signup/route.ts
+++ app/api/users/signup/route.ts
@@ -2,33 +2,30 @@ import { connect } from "@/dbConfig/dbConfig";
 import User from "@/models/userModel";
 import { NextRequest, NextResponse } from "next/server";
 
-import brcrypt from 'bcryptjs'
+import brcryptjs from 'bcryptjs'
 import { sendEmail } from "@/helpers/mailer";
 
 connect()
 export async function POST(request: NextRequest) {
     try {
-        const reqBody = request.json();
-        const { email, usename, password } = reqBody;
-
-        const user = await User.findOne(email);
-        if (!user) {
-            return NextResponse.json({ error: 'User already exists, please login!' })
+        const reqBody = await request.json();
+        const { email, username, password } = reqBody;
+        const user = await User.findOne({ email });
+        if (user) {
+            return NextResponse.json({ error: 'User already exists, please login!!!' })
         }
-        const salt = await brcrypt.getSalt('10');
-        const hashedPass = await brcrypt.hash(password, salt);
+        const salt = await brcryptjs.genSalt(10);
+
+        const hashedPass = await brcryptjs.hash(password, salt);
 
         const newUser = new User({
             email,
-            usename,
+            username,
             password: hashedPass
         })
+        const savedUser = await newUser.save();
 
-        const savedUser = newUser.save();
-        console.log(savedUser);
-        // Send verification email
-
-        const emailInfo = await sendEmail({ email, emailSubject: 'Verify Email', userId: savedUser?._id });
+        const emailInfo = await sendEmail({ email, emailType: 'VERIFY', userId: savedUser?._id });
 
         return NextResponse.json({
             message: 'User Registered Successfully',
--- helpers/mailer.ts
+++ helpers/mailer.ts
@@ -1,19 +1,39 @@
 import nodemailer from 'nodemailer'
-
-export const sendEmail = async ({ email, emailSubject, userId }: { email: string, emailSubject: string, userId: string }) => {
+import bcryptjs from 'bcryptjs'
+import User from '@/models/userModel';
+export const sendEmail = async ({ email, emailType, userId }: { email: string, emailType: string, userId: string }) => {
     try {
-        const transporter = nodemailer.createTransport({
-            host: "smtp.forwardemail.net",
-            port: 465,
-            secure: false, // true for port 465, false for other ports
-        })
 
-        const info = await transporter.sendMail({
+        const hashedToken = await bcryptjs.hash(userId.toString(), 10);
+        if (emailType === 'VERIFY') {
+            await User.findByIdAndUpdate(userId, {
+                verifyToken: hashedToken,
+                verifyTokenExpiry: Date.now() + 3600000
+            })
+        } else if (emailType === 'RESET') {
+            await User.findByIdAndUpdate(userId, {
+                forgorPasswordToken: hashedToken,
+                forgorPasswordTokenExpiry: Date.now() + 3600000
+            })
+        }
+
+        // Looking to send emails in production? Check out our Email API/SMTP product!
+        var transport = nodemailer.createTransport({
+            host: "sandbox.smtp.mailtrap.io",
+            port: 2525,
+            auth: {
+                user: process.env.EMAIL_AUTH_USER, // this will be in your env file
+                pass: process.env.EMAIL_AUTH_PASS  // this will be in your env file
+            }
+        });
+
+        const info = await transport.sendMail({
             from: 'pushpraj@crownstack.com', // sender address
             to: email, // list of receivers
-            subject: emailSubject, // Subject line
+            subject: emailType === 'VERIFY' ? 'Verify you email' : 'Reset your password', // Subject line
             text: "Hello world?", // plain text body
-            html: "<b>Hello world?</b>", // html body
+            html: `<p>Click <a href="${process.env.DOMAIL_URL}}/verifyemail?token=${hashedToken}">here</a> to ${emailType === 'VERIFY' ? 'verify your email address' : 'reset your account password'}
+            or copy and paste the below link in your browser. <br> ${process.env.DOMAIL_URL}}/verifyemail?token=${hashedToken} </br> </p>`,
         });
 
         console.log("Message sent: %s", info.messageId);
